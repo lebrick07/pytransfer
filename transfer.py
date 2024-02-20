5b1d21dc078abd7a218afe08ddc3c1b92d8e3f2f
Idan Ben Yair <iyair@autometalabs.com>
2024-02-20 14:09:03 -0500

    Adding progress bar

--- transfer.py
+++ transfer.py
@@ -2,11 +2,11 @@ import os
 from googleapiclient.discovery import build
 from google.oauth2 import service_account
 import boto3
+from tqdm import tqdm
 
 # Google Drive API setup
 SCOPES = ['https://www.googleapis.com/auth/drive.readonly']
 SERVICE_ACCOUNT_FILE = 'credentials.json'  # Path to your service account credentials file
-DRIVE_FOLDER_ID = 'YOUR_GOOGLE_DRIVE_FOLDER_ID'  # ID of the Google Drive folder containing the file
 
 # AWS S3 setup
 AWS_ACCESS_KEY_ID = 'YOUR_AWS_ACCESS_KEY_ID'
@@ -22,13 +22,33 @@ s3 = boto3.client('s3', aws_access_key_id=AWS_ACCESS_KEY_ID, aws_secret_access_k
 
 def download_file_from_google_drive(file_id, file_name):
     request = service.files().get_media(fileId=file_id)
-    fh = open(file_name, 'wb')
-    downloader = request.execute()
-    fh.write(downloader)
-    fh.close()
+    with open(file_name, 'wb') as fh:
+        downloader = tqdm(desc="Downloading", unit="B", unit_scale=True)
+        response = request.execute()
+        for chunk in response:
+            if isinstance(chunk, int):
+                chunk = chunk.to_bytes((chunk.bit_length() + 7) // 8, 'big')
+            if chunk:
+                fh.write(chunk)
+                downloader.update(len(chunk))
+
 
 def upload_file_to_s3(file_name):
-    s3.upload_file(file_name, S3_BUCKET_NAME, os.path.basename(file_name))
+    file_size = os.path.getsize(file_name)
+    with open(file_name, 'rb') as fh:
+        s3.upload_fileobj(fh, S3_BUCKET_NAME, os.path.basename(file_name), Callback=ProgressPercentage(file_size))
+
+class ProgressPercentage(object):
+    def __init__(self, file_size):
+        self._file_size = file_size
+        self._seen_so_far = 0
+        self._lock = tqdm.get_lock()
+        self._pbar = tqdm(total=file_size, desc="Uploading", unit="B", unit_scale=True)
+
+    def __call__(self, bytes_amount):
+        with self._lock:
+            self._seen_so_far += bytes_amount
+            self._pbar.update(bytes_amount)
 
 def main():
     file_id = input("Enter the Google Drive file ID: ")
